cc4a5cfc8ac8fede5a034cabc8392bb1d80d0364
Only1Afoo <sheriffdeenolawale@gmail.com>
2022-12-23 05:16:58 -0800

    Adv Task 1

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -3,13 +3,13 @@
  /**
   * main - prints all possible different combinations of two digits
   *
-  * Return: Always 0 (success)
-  *
+  *Return: Always 0 (Success)
   */
 
 int main(void)
 
 {
+
 int ones = '0';
 int tens = '0';
 
