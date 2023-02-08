adf316bb9ca670bbf78c80c62794088fd45ca190
Only1Afoo <sheriffdeenolawale@gmail.com>
2023-02-07 22:15:17 -0800

    Adv task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,8 +7,8 @@
 
 int main(void)
 {
-  int ones = '0';
-  int tens = '0';
+int ones = '0';
+int tens = '0';
 
 for (tens = '0'; tens <= '9'; tens++)/* prints tens digit*/
 {
@@ -16,16 +16,16 @@ for (ones = '0'; ones <= '9'; ones++)/* prints ones digit*/
 {
 if (!((ones == tens) || (tens > ones)))/*eliminates repitition*/
 {
-   putchar(tens);
-   putchar(ones);
+putchar(tens);
+putchar(ones);
 if (!(ones == '9' && tens == '8'))/*addes commma and space*/
 
- putchar(',');
- putchar(' ');
+putchar(',');
+putchar(' ');
 }
- }
-  }
-   }
-  putchar('\n');
-  return (0);
+}
+}
+}
+putchar('\n');
+return (0);
 }
